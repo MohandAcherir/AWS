2602eb0d6d1fae70ce50be7cab93ab37b033366e
firas baya <firasbaya@hotmail.com>
2023-04-12 22:37:35 +0200

    Modifications home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,24 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './sharepage/navbar/navbar.component';
-import { FooterComponent } from './sharepage/footer/footer.component';
-import { HomeComponent } from './pages/home/home.component';
-import { MenuComponent } from './pages/menu/menu.component';
-import { AboutComponent } from './pages/about/about.component';
-import { ContactComponent } from './pages/contact/contact.component';
-import { ProductsComponent } from './pages/products/products.component';
-import { MenupageComponent } from './pages/menupage/menupage.component';
-
+import { ImcCalculatorComponent } from './im-calcul/im-calcul.component';
+import { CalorieCalculComponent } from './calorie-calcul/calorie-calcul.component';
+import {ProductsComponent} from './products/products.component';
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    FooterComponent,
-    MenuComponent,
-    AboutComponent,
-    ContactComponent,
+    ImcCalculatorComponent,
+    CalorieCalculComponent,
     ProductsComponent,
-    MenupageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
