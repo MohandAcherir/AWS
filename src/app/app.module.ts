de12f56ec93505c6babce32ddc7337636f9686ae
MohandAcherir <mihawk7@outlook.com>
2023-04-13 15:10:39 +0200

    Ajout de la partie de Firas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { FormsModule } from '@angular/forms';
-
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -70,7 +71,9 @@ const routes = [
     RouterModule.forRoot(routes),
     MatInputModule,
     MatListModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatIconModule
   ],
   providers: [ApiService, AuthService, {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true}],
   bootstrap: [AppComponent]
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,53 @@
 
 import { Component, OnInit } from '@angular/core';
 import { ExerciceDetailsService } from 'src/app/services/exercice-details.service';
+import { CalorieCalculComponent } from 'src/app/services/calorie-calcul.component';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  constructor(private service:ExerciceDetailsService){}
-  exerciceData:any;
+  weight: number  = 0;
+  height: number = 0;
+  age: number = 0;
+  gender: string = "";
+  activity: string = "";
+  bmr: number = 0;
+  tdee: number = 0;
+  protein: number = 0;
+  fat: number= 0;
+  carbs: number = 0;
+  showResults: boolean = true; // Define the showResults property
+ 
+  constructor(private service:CalorieCalculComponent){
+      this.weight = this.service.weight;
+    this.height = this.service.height;
+    this.age = this.service.age;
+    this.gender = this.service.gender;
+    this.activity = this.service.activity;
+    this.bmr = this.service.bmr;
+    this.tdee = this.service.tdee;
+    this.protein = this.service.protein;
+    this.fat= this.service.fat;
+    this.carbs = this.service.carbs;
+    this.showResults = true;
+  }  
   ngOnInit(): void {
-    this.exerciceData=this.service.exerciceDetails;
+    this.weight = 0;
+    this.height = 0;
+    this.age = 0;
+    this.gender = "";
+    this.activity = "";
+    this.bmr = 0;
+    this.tdee = 0;
+    this.protein = 0;
+    this.fat=0;
+    this.carbs = 0;
+    this.showResults = true; // Define the showResults property
+  }
+  
+  calculate():number {
+  	return 2;
   }
 }
